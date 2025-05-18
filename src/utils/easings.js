693725ff194abb982e3b8a49c365b3be724e6a48
‪Itamar Zand‬‏ <133867530+ItamarZand88@users.noreply.github.com>
2025-05-18 10:56:44 +0300

    Add easings utility

---
+++ src/utils/easings.js
@@ -0,0 +1,26 @@
+// Custom easing functions for more animation variety
+export const bounceOut = "bounce.out";
+export const bounceIn = "bounce.in";
+export const bounceInOut = "bounce.inOut";
+
+// Elastic
+export const elastic = "elastic.out(1, 0.3)";
+export const elasticIn = "elastic.in(1, 0.3)";
+export const elasticOut = "elastic.out(1, 0.3)";
+
+// Back
+export const backIn = "back.in(1.7)";
+export const backOut = "back.out(1.7)";
+export const backInOut = "back.inOut(1.7)";
+
+// Slow motion
+export const slowMo = "slow(0.5, 0.7)";
+
+// Steps
+export const roughSteps = "steps(8)";
+export const smoothSteps = "steps(20)";
+
+// Custom
+export const softBounce = "power2.out";
+export const gentleElastic = "elastic.out(1, 0.2)";
+export const quickSnap = "power4.inOut";
